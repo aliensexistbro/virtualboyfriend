60ba9b8afc2a707171497da1c7f835509b61af00
Ashtrayyam <jeremyyan1999@hotmail.com>
2023-11-27 16:53:24 -0800

    dialoguesystem

---
+++ Assets/Scripts/startConvo.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DialogueEditor;
+
+public class startConvo : MonoBehaviour
+{
+    [SerializeField] private NPCConversation myConvo;
+    // Start is called before the first frame update
+    void Start(){
+        ConversationManager.Instance.StartConversation(myConvo);
+    }
+    // private void OnTriggerEnter(Collider other){
+    //     if(other.CompareTag("Player")){
+            
+    //         ConversationManager.Instance.StartConversation(myConvo);
+    //     }
+    // }
+}
