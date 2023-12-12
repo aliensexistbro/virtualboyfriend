45e41f801e91b62367d21b7f8d7e7dae609c2baa
Ashtrayyam <jeremyyan1999@hotmail.com>
2023-12-11 23:22:08 -0800

    sceneloader script update/change scene after dialgue end

--- 445-VirtualBoyfriend-ver11.28/Assets/Scripts/SceneLoader.cs
+++ 445-VirtualBoyfriend-ver11.28/Assets/Scripts/SceneLoader.cs
@@ -2,13 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using DialogueEditor;
 public class SceneLoader : MonoBehaviour
 {
     public string sceneName;
     public Animator transition;
     public float transitionTime = 1f;
     bool loadingStarted = false;
+    private bool sceneReady=false;
     float secondsLeft = 0;
     // Update is called once per frame
     public void ExitGame()
@@ -24,7 +25,16 @@ public class SceneLoader : MonoBehaviour
         SceneManager.LoadScene(sceneName);
         Debug.Log("playing");
     }
+    public void Update(){
+        if(ConversationManager.Instance.IsConversationActive)
+            sceneReady=ConversationManager.Instance.GetBool("SceneChangeReady");
 
+        if(sceneReady){
+            Debug.Log(sceneReady);
+            sceneReady=false;
+            TimeLoad(3f);
+        }
+    }
     public void LoadScene()
     {
         StartCoroutine(FadeScene(sceneName));
